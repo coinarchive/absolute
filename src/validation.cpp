20e4063914a0e07a57c0251397103537eb1d2236
Crypto Centric <cryptocentric@absolutecoin.net>
2020-02-16 13:50:11 +0000

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -4234,6 +4234,7 @@ bool LoadExternalBlockFile(const CChainParams& chainparams, FILE* fileIn, CDiskB
                     while (range.first != range.second) {
                         std::multimap<uint256, CDiskBlockPos>::iterator it = range.first;
                         std::shared_ptr<CBlock> pblockrecursive = std::make_shared<CBlock>();
+			const int nHeight = mapBlockIndex[it->first]->nHeight;
                         if (ReadBlockFromDisk(*pblockrecursive, it->second, chainparams.GetConsensus()))
                         {
                             LogPrint("reindex", "%s: Processing out of order child %s of %s\n", __func__, pblockrecursive->GetHash().ToString(),
